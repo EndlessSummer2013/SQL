e1578b792663877e43bb2e36954a56a3801094f8
李翔 <lixiang@lixiang.local>
2013-08-21 11:01:00 +0800

    Initial Commit

---
+++ SQL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SQL
+//
+//  Created by 李翔 on 13-8-21.
+//  Copyright (c) 2013年 李翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
